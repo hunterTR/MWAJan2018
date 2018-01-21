546e489ad16a894aaaea1e25381cf273fcb0f1d2
Cem KAYA <ahmetcem3@gmail.com>
2018-01-21 16:12:16 -0600

    assignment 6 cors

--- assignment6/exercise/app.js
+++ assignment6/exercise/app.js
@@ -25,6 +25,12 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/', index);
 app.use('/grades', grades);
 
